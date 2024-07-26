49803e14acc8240f313c7dde5107fbcfef0491d2
Velizara Sotirova <velizarasotirova@gmail.com>
2024-07-26 11:04:57 +0300

    Solutions added

---
+++ JSON-Processing/ProductShop/ProductShop/Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace ProductShop.Data
+{
+    public static class Configuration
+    {
+        public const string ConnectionString =
+            @"Server=DESKTOP-DOQUI53\SQLEXPRESS;Database=ProductShop;Integrated Security=True";
+    }
+}
